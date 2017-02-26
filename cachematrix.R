6e270808c7bd83b8839b88778320317a7c2ab60a
juliascheel <julia.scheel@gmx.net>
2017-02-26 19:28:17 +0100

    R assignment 2: 2 Functions. Inversing and Cacheing

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,39 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Two functions: Inverse Matrix and Cache Inverse Matrix
+## Function 1: creates special "matrix"objects that can cache its inverse
 
+#' @param x 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+makeCacheMatrix <- function(x = matrix()) {
+  inverse <- NULL
+  f <- function(y) {
+    x <<- y
+    inverse <<- NULL 
+  }
+  get <- function() x 
+  finverse <- function(solveMatrix) inverse <<- solveMatrix
+  finalinverse <- function () inverse 
+  list(f = f, get = get, finverse = finverse, finalinverse = finalinverse )
 }
 
 
-## Write a short comment describing this function
+## Function 2 computes the inverse of the "matrix" calculated in function 1
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <- x$finalinverse()
+  if(!is.null(inverse)){
+    message("get cached inversed matrix")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data)
+  x$finverse(inverse)
+  inverse
 }
